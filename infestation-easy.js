15b647e7f98be2aefaa51033147b3fce6fbfdaaa
Glitch&Go <ritter.oxley1@gmail.com>
2025-07-26 17:55:57 -0500

    Update infestation-easy.js

--- infestation-easy.js
+++ infestation-easy.js
@@ -57,7 +57,7 @@ export function startEasyMode() {
 
     gameArea.appendChild(bug);
 
-    const bugSpeed = 4000;
+    const bugSpeed = 6000;
     bug.animate(
       [
         { transform: 'translateX(0)' },
